4c88089433c8876cf604fc19a8a42eedf4fba2f5
oldfish <oldfish101240@gmail.com>
2025-04-12 18:46:07 +0800

    新的檔案環境(配合新版)

---
+++ src/start.bat
@@ -0,0 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB1dXwzSAE+1EbsQ5+n//NaIrkkbXfE3R4vUzbqCL+EX71fYeJck2EZbi9gJGAhkdxGkYEE9qmEi
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQI/JB9SRReKubZrgVz7bDAQZzZuJ7TVNA==
+::eg0/rx1wNQPfEVWB+kM9LVsJDAuOOG2zFb0j7Ofs4eaIo0kYRoI=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQK3+NrRusoG5IAy2lwl5/3P+uiCJ5DM0wvY91M6ug==
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB1dXwzSAE+1EbsQ5+n//NaIrkkbXfE3R4vUzbqCL+EX71fYepop0HNJkE7R3ZyxmZZzhYfoQOu7fiqAL8L8
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+"%~dp0python_embed\pythonw.exe" "%~dp0main\oldfish_downloader_rebuild.pyw"
+pause
